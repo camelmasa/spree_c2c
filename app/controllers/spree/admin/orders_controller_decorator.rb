9f5ececad9e158980575d54d3474757f454e1a29
Masahiro Saito <camelmasa@gmail.com>
2012-08-30 21:40:06 +0900

    add orders c2c display

---
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -0,0 +1,43 @@
+Spree::Admin::OrdersController.class_eval do
+
+  def index
+    params[:q] ||= {}
+    params[:q][:completed_at_not_null] ||= '1' if Spree::Config[:show_only_complete_orders_by_default]
+    @show_only_completed = params[:q][:completed_at_not_null].present?
+    params[:q][:s] ||= @show_only_completed ? 'completed_at desc' : 'created_at desc'
+
+    # As date params are deleted if @show_only_completed, store
+    # the original date so we can restore them into the params
+    # after the search
+    created_at_gt = params[:q][:created_at_gt]
+    created_at_lt = params[:q][:created_at_lt]
+
+    if !params[:q][:created_at_gt].blank?
+      params[:q][:created_at_gt] = Time.zone.parse(params[:q][:created_at_gt]).beginning_of_day rescue ""
+    end
+
+    if !params[:q][:created_at_lt].blank?
+      params[:q][:created_at_lt] = Time.zone.parse(params[:q][:created_at_lt]).end_of_day rescue ""
+    end
+
+    if @show_only_completed
+      params[:q][:completed_at_gt] = params[:q].delete(:created_at_gt)
+      params[:q][:completed_at_lt] = params[:q].delete(:created_at_lt)
+    end
+
+    object = Spree::Order
+    roles = current_user.spree_roles.map do |role| role.name end
+    object = object.where(:product_user_id => current_user.id) unless roles.include?("admin")
+    @search = object.ransack(params[:q])
+    @orders = @search.result.includes([:user, :shipments, :payments]).page(params[:page]).per(Spree::Config[:orders_per_page])
+
+    # Restore dates
+    params[:q][:created_at_gt] = created_at_gt
+    params[:q][:created_at_lt] = created_at_lt
+
+    respond_with(@orders)
+  end
+
+
+end
+
--- db/migrate/20120830172620_add_user_id_field_to_products.rb
+++ db/migrate/20120830172620_add_user_id_field_to_products.rb
@@ -1,6 +1,7 @@
 class AddUserIdFieldToProducts < ActiveRecord::Migration
   def up
     add_column :spree_products, :user_id, :integer, :default => 0, :null => false
+    add_index "spree_products", ["user_id"], :name => "index_spree_orders_on_user_id"
   end
 
   def down
