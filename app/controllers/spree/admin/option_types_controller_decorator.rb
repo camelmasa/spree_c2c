eac25451b284069e08848d44ba749138acc67d6d
Masahiro Saito <camelmasa@gmail.com>
2012-08-31 01:27:24 +0900

    add decorators

---
+++ app/controllers/spree/admin/option_types_controller_decorator.rb
@@ -0,0 +1,16 @@
+Spree::Admin::OptionTypesController.class_eval do
+
+  def index
+    roles = current_user.spree_roles.map do |role| role.name end
+    if member_action?
+      @object ||= load_resource_instance
+      @object = @object.where(:user_id => current_user.id) unless roles.include?("admin")
+      instance_variable_set("@#{object_name}", @object)
+    else
+      @collection ||= collection
+      @collection = @collection.where(:user_id => current_user.id) unless roles.include?("admin")
+      instance_variable_set("@#{controller_name}", @collection)
+    end
+  end
+
+end
