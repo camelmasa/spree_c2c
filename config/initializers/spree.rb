7fa6432ab43d61afcc628e6bfefbb587cf72034c
Masahiro Saito <camelmasa@gmail.com>
2012-08-30 18:02:08 +0900

    add initializers

---
+++ config/initializers/spree.rb
@@ -0,0 +1,12 @@
+class ConsumerAbility
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= Spree.user_class.new
+    if user.respond_to?(:has_spree_role?) && user.has_spree_role?('consumer')
+      can :manage, :all
+    end
+  end
+end
+
+Spree::Ability.register_ability(ConsumerAbility)
