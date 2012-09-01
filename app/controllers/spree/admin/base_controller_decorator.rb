08f3c40cd7d15c8a8a657fd578d19596f3612272
Masahiro Saito <camelmasa@gmail.com>
2012-09-01 18:09:34 +0900

    some controllers update

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -1,5 +1,15 @@
 Spree::Admin::BaseController.class_eval do
 
+  before_filter :only_admin_role
+
+  def only_admin_role
+
+    controller_names = ['reports', 'configurations', 'general_settings', 'mail_methods', 'tax_categories', 'zones', 'countries', 'states',  'payment_methods', 'taxonomies', 'shipping_methods', 'shipping_categories', 'inventory_settings', 'image_settings', 'tax_rates', 'tax_settings', 'trackers', 'promotions', 'users']
+
+    return head(401) if !admin? && controller_names.include?(controller_name)
+
+  end
+
   def admin?
     roles.include?("admin")
   end
@@ -7,10 +17,6 @@ Spree::Admin::BaseController.class_eval do
   def seller?
     return true if admin?
     roles.include?("seller")
-  end
-
-  def check_admin
-    return head(400) if !admin?
   end
 
   private
--- app/controllers/spree/admin/reports_controller_decorator.rb
+++
@@ -1,6 +0,0 @@
-Spree::Admin::ReportsController.class_eval do 
-
-  before_filter :check_admin
-
-end
-             
