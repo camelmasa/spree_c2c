93af97cb541b7f7f0c3c0f89cd8a527120d64670
Masahiro Saito <camelmasa@gmail.com>
2012-09-05 18:25:00 +0900

    bug fix

--- app/controllers/spree/admin/resource_controller_decorator.rb
+++ app/controllers/spree/admin/resource_controller_decorator.rb
@@ -22,7 +22,7 @@ Spree::Admin::ResourceController.class_eval do
   def load_resource
     if member_action?
       @object ||= load_resource_instance
-      return head(401) if !admin? && (@object.user_id != current_user.id)
+      return head(401) if !admin? && (!@object.user_id.zero? && @object.user_id != current_user.id)
       instance_variable_set("@#{object_name}", @object)
     else
       @collection ||= collection
